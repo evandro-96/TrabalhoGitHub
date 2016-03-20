2ee3d71e468ec79f36f68ed5a9c8d1db4f5a25cb
Evandro Pimentel de Areis <evandro-96@live.com>
2016-03-19 21:54:12 -0300

    Trabalho Commit

---
+++ br/univel/classeabstrata/DesenhoAbstrato.java
@@ -0,0 +1,17 @@
+package br.univel.classeabstrata;
+
+import br.univel.interfaces.Desenho;
+
+abstract class DesenhoAbstrato implements Desenho {
+
+	public void identificar() {
+		System.out.println("===================================");
+		System.out.println(getNome());
+		System.out.println("===================================");
+		System.out.println("\n");
+	}
+	
+	protected abstract String getNome();
+	
+	
+}
